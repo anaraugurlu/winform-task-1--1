e17c544e8938ea0f07e8e44d407df22872b418e6
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-06-09 09:49:26 +0400

    Добавьте файлы проекта.

---
+++ winform task 1 -1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_task_1__1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sender is Button b)
+            {
+                b.Dispose();
+                Button button = new Button();
+               
+                Random random = new Random();
+                int randomW = random.Next(this.Size.Width - 100);
+                int randomH = random.Next(this.Size.Height - 100);
+                button.Size = new Size(120, 30);
+                button.BackColor = Color.Transparent;
+                button.ForeColor = Color.Magenta;
+                button.Location = new Point(randomW, randomH);
+                button.Text = "Start";
+                button.MouseEnter += button1_Click;
+
+                this.Controls.Add(button);
+
+            }
+        }
+    }
+}
